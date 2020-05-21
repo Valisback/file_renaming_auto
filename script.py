e1ab11e606e935194c9695ed917c1d054c83b279
Valentin Pereira <49312215+Valisback@users.noreply.github.com>
2020-05-21 10:48:04 +0200

    Generalized to any pattern 🚀

--- script.py
+++ script.py
@@ -1,13 +1,15 @@
 import os
 
 folder_name = input('Folder Name: ')
+pattern = input('Enter pattern to remove: ')
+
 
 folder_name = folder_name + '/'
 
 with os.scandir(folder_name) as entries:
     for entry in entries:
-        if( 'IMG-' in entry.name):
-            size = len(folder_name) + len('IMG-')
+        if( pattern in entry.name):
+            size = len(folder_name) + len(pattern)
             new_entry = entry.path[size:]
             new_entry = folder_name + new_entry
             os.rename(entry.path, new_entry)
